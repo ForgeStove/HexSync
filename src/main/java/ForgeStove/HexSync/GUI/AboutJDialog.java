1d66e4bc4f312e9c4474f841e8c7b11c82a19557
ForgeStove <2071948808@qq.com>
2025-01-24 12:07:07 +0800

    fix(ui): 更新许可证链接并简化代码

--- src/main/java/ForgeStove/HexSync/GUI/AboutJDialog.java
+++ src/main/java/ForgeStove/HexSync/GUI/AboutJDialog.java
@@ -13,13 +13,9 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 package ForgeStove.HexSync.GUI;
-import ForgeStove.HexSync.HexSync;
-
 import javax.swing.*;
 import javax.swing.event.HyperlinkEvent;
 import java.awt.*;
-import java.io.*;
-import java.util.Objects;
 
 import static ForgeStove.HexSync.GUI.ComponentFactory.*;
 import static ForgeStove.HexSync.HexSync.*;
@@ -39,31 +35,12 @@ public class AboutJDialog {
 				+ GITHUB_URL
 				+ "\">"
 				+ GITHUB_URL
-				+ "</a><br>开源许可: <a href=\"file:LICENSE\">GNU General Public License v3.0</a></span>");
+				+ "</a><br>开源许可: <a href=\""
+				+ GITHUB_URL
+				+ "/blob/main/LICENSE\">GNU General Public License v3.0</a></span>");
 		aboutTextPane.addHyperlinkListener(event -> {
 			if (HyperlinkEvent.EventType.ACTIVATED.equals(event.getEventType())) try {
-				String url = event.getURL().toString();
-				if (url.equals(GITHUB_URL)) {
-					Desktop.getDesktop().browse(event.getURL().toURI());
-				} else if (url.equals("file:LICENSE")) {
-					if (checkJDialog("许可证")) return;
-					try (
-							BufferedReader reader = new BufferedReader(new InputStreamReader(Objects.requireNonNull(
-									HexSync.class.getClassLoader().getResourceAsStream("LICENSE"))))
-					) {
-						StringBuilder licenseContent = new StringBuilder();
-						String line;
-						while ((line = reader.readLine()) != null)
-							licenseContent.append(line).append(System.lineSeparator());
-						JTextArea licenseTextArea = new JTextArea(licenseContent.toString());
-						licenseTextArea.setEditable(false);
-						licenseTextArea.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-						JDialog licenseJDialog = new JDialog(aboutDialog, "许可证");
-						licenseJDialog.add(new JScrollPane(licenseTextArea));
-						licenseJDialog.pack();
-						setWindow(licenseJDialog);
-					}
-				}
+				Desktop.getDesktop().browse(event.getURL().toURI());
 			} catch (Exception error) {
 				log(WARNING, "无法打开超链接: " + error.getMessage());
 			}
